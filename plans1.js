7520b0f66c0386aa3e7af763ef7a9900de5154af
mariaclarin <maria.clarin@binus.ac.id>
2022-01-05 23:15:42 +0700

    updated planning pages (scenario based)

--- plans1.js
+++ plans1.js
@@ -1,38 +1,3 @@
-let btnRegular = document.querySelector('#regular');
-let btnVegan = document.querySelector('#vegan');
-let btnPescatarian = document.querySelector('#pescatarian');
-let btnKeto = document.querySelector('#keto');
-
-btnRegular.addEventListener('click', () => btnRegular.style.backgroundColor = '#A3C193')
-btnVegan.addEventListener('click', () => btnVegan.style.backgroundColor = '#A3C193')
-btnPescatarian.addEventListener('click', () => btnPescatarian.style.backgroundColor = '#A3C193')
-btnKeto.addEventListener('click', () => btnKeto.style.backgroundColor = '#A3C193')
-
-let btnSingle = document.querySelector('#single');
-let btnDuo = document.querySelector('#duo');
-let btnFamily = document.querySelector('#family');
-
-
-btnSingle.addEventListener('click', () => btnSingle.style.backgroundColor = '#A3C193')
-btnDuo.addEventListener('click', () => btnDuo.style.backgroundColor = '#A3C193')
-btnFamily.addEventListener('click', () => btnFamily.style.backgroundColor = '#A3C193')
-
-
-let btnTwo = document.querySelector('#two');
-let btnThree = document.querySelector('#three');
-let btnFour = document.querySelector('#four');
-let btnFive = document.querySelector('#five');
-let btnSix = document.querySelector('#six');
-let btnSeven = document.querySelector('#seven');
-
-
-btnTwo.addEventListener('click', () => btnTwo.style.backgroundColor = '#A3C193')
-btnThree.addEventListener('click', () => btnThree.style.backgroundColor = '#A3C193')
-btnFour.addEventListener('click', () => btnFour.style.backgroundColor = '#A3C193')
-btnFive.addEventListener('click', () => btnFive.style.backgroundColor = '#A3C193')
-btnSix.addEventListener('click', () => btnSix.style.backgroundColor = '#A3C193')
-btnSeven.addEventListener('click', () => btnSeven.style.backgroundColor = '#A3C193')
-
 let btnChicken = document.querySelector('#chicken');
 let btnBeef = document.querySelector('#beef');
 let btnPork = document.querySelector('#pork');
@@ -40,6 +5,7 @@ let btnNuts = document.querySelector('#nuts');
 let btnSeafood = document.querySelector('#seafood');
 let btnDairy = document.querySelector('#dairy');
 let btnShellfish = document.querySelector('#shellfish');
+let btnNone = document.querySelector('#none');
 
 
 btnChicken.addEventListener('click', () => btnChicken.style.backgroundColor = '#A3C193')
@@ -49,3 +15,4 @@ btnNuts.addEventListener('click', () => btnNuts.style.backgroundColor = '#A3C193
 btnSeafood.addEventListener('click', () => btnSeafood.style.backgroundColor = '#A3C193')
 btnShellfish.addEventListener('click', () => btnShellfish.style.backgroundColor = '#A3C193')
 btnDairy.addEventListener('click', () => btnDairy.style.backgroundColor = '#A3C193')
+btnNone.addEventListener('click', () => btnNone.style.backgroundColor = '#A3C193')
